6f0bee76a12990acc4ac3eda093b9a4d92c8f239
EEB12 <rayhan.taqara@yahoo.com>
2021-11-29 04:36:34 +0700

    ngarah ke shop

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,9 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/shop', function () {
+    return view('profile.shop');
+})->middleware(['auth'])->name('shop');
 
 require __DIR__.'/auth.php';
 
-Route::get('/shop', function () {
-    return view('profile.shop');
-});
